c0b9147eea97446e53b78f4c51dbec45ac093e5c
Olha Chornei <pradocw8053@gmail.com>
2022-11-22 17:36:28 +0200

    Update images-api.js

--- src/services/images-api.js
+++ src/services/images-api.js
@@ -2,7 +2,7 @@ const URL = 'https://pixabay.com/api/';
 const KEY = '11240134-58b8f655e9e0f8ae8b6e8e7de';
 const FILTER = '&image_type=photo&orientation=horizontal&per_page=12';
 
-function fetch(queryParams, page = 1) {
+function fetch(queryParams, page) {
   return fetch(`${URL}?q=${queryParams}&page=${page}&key=${KEY}${FILTER}`).then(
     response => response.json()
   );
